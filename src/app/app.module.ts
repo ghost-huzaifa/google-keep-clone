3b6276dfa158803ba8e674e923b613eb21f7e5b7
Muhammad Huzaifa Mughal <124766345+ghost-huzaifa@users.noreply.github.com>
2024-07-30 15:24:55 +0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteTakerComponent } from './note-taker/note-taker.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteTakerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/note-taker/note-taker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'note-taker',
+  templateUrl: './note-taker.component.html',
+  styleUrl: './note-taker.component.css'
+})
+export class NoteTakerComponent {
+
+}
